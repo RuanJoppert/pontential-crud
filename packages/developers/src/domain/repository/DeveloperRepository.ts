0105cb07a87f0da3068787474d582611ffdfac6d
Ruan Joppert <ruan.joppert@gmail.com>
2020-10-18 14:29:08 -0300

    refactor: change DeveloperRepository interface

--- packages/developers/src/domain/repository/DeveloperRepository.ts
+++ packages/developers/src/domain/repository/DeveloperRepository.ts
@@ -1,10 +1,10 @@
 import { Developer, DeveloperProps } from '../entity/Developer'
 
-export interface DeveloperRepository {
-  create(developer: DeveloperProps, id?: string): Promise<Developer | Error[]>
-  update(developer: DeveloperProps, id: string): Promise<true | null | Error[]>
-  removeById(id: string): Promise<true | null>
+export interface IDeveloperRepository {
+  create(developer: DeveloperProps, id?: string): Promise<Developer>
+  update(developer: Partial<DeveloperProps>, id: string): Promise<Developer>
+  removeById(id: string): Promise<boolean>
 
-  findById(id: string): Promise<Developer | null>
-  find(filter: Partial<DeveloperProps>, offset: string, size: string): Promise<Developer[]>
+  findById(id: string): Promise<Developer>
+  find(filter?: Partial<DeveloperProps>, offset?: number, limit?: number): Promise<Developer[]>
 }
