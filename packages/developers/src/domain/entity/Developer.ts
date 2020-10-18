1dcca875ef4be9f883f1090abbcad3872b6cebe4
Ruan Joppert <ruan.joppert@gmail.com>
2020-10-17 21:19:04 -0300

    feat: add Developer entity

---
+++ packages/developers/src/domain/entity/Developer.ts
@@ -0,0 +1,92 @@
+import { Result } from '@potential-crud/result'
+import { DataNascimento, Nome, Sexo, UniqueEntityID } from '../objectValues'
+
+export interface DeveloperProps {
+  nome: string
+  sexo: 'M' | 'F'
+  hobby?: string
+  datanascimento: Date | string
+}
+
+export class Developer {
+  private readonly _id: string
+
+  public nome: string
+  public sexo: 'M' | 'F'
+  public hobby?: string
+  public datanascimento: Date
+
+  get id() {
+    return this._id
+  }
+
+  get idade() {
+    const today = new Date()
+    const birth = this.datanascimento
+    const monthDifference = today.getMonth() - birth.getMonth()
+
+    let age = today.getFullYear() - birth.getFullYear()
+
+    if (monthDifference < 0 || (monthDifference === 0 && today.getDate() < birth.getDate())) {
+      age--
+    }
+
+    return age
+  }
+
+  private constructor(
+    props: {
+      nome: string
+      sexo: 'M' | 'F'
+      hobby?: string
+      datanascimento: Date
+    },
+    id: string
+  ) {
+    this._id = id
+
+    this.nome = props.nome
+    this.sexo = props.sexo
+    this.hobby = props.hobby
+    this.datanascimento = props.datanascimento
+  }
+
+  static create(props: DeveloperProps, id?: string) {
+    const result = Result.from(
+      Nome.create(props.nome),
+      Sexo.create(props.sexo),
+      DataNascimento.create(props.datanascimento),
+      props.hobby
+    )
+
+    if (result.fail === true) {
+      return Result.fail(result.error)
+    }
+
+    const [nome, sexo, datanascimento, hobby] = result.unwrap()
+
+    if (!id) {
+      const uuid = UniqueEntityID.create()
+
+      if (uuid.fail === true) {
+        return Result.fail([uuid.error])
+      }
+
+      id = uuid.value.value
+    }
+
+    const dev = new Developer(
+      {
+        nome: nome.value,
+        sexo: sexo.value,
+        datanascimento: datanascimento.value,
+        hobby
+      },
+      id
+    )
+
+    return Result.ok(dev)
+  }
+
+  static update() {}
+}
