110eaed55a1ab2c2ecb2b1becc0bb0cf0e24be32
Ruan Joppert <ruan.joppert@gmail.com>
2020-10-21 15:50:44 -0300

    test: fix environment

--- developers/setupTests.ts
+++ developers/setupTests.ts
@@ -1,8 +1,4 @@
 import dotenv from 'dotenv'
 import { join } from 'path'
 
-<<<<<<< HEAD
 dotenv.config({ path: join(__dirname, './.example.test.env') })
-=======
-dotenv.config({ path: join(__dirname, './.example.env') })
->>>>>>> 42b2f9a3552a4ef0780cb4a3c2ab31537549eff6
